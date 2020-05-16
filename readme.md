b5ef86ddeee9d9299d76d7438c0c68aeacdd98b2
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-15 23:32:16 -0300

    Refactoring the emscripten's example

--- readme.md
+++ readme.md
@@ -1,10 +1,21 @@
 #Hello SDL
 The next step is to see something of the SDL library on screen.
 
-## Testing if I am compiling in an emscripten environment
+##Emscritpten Envoronment
+
+### Toolchain file
+Instead of using emcmake/emmake I can use the toolchain file I found. Just remember to 
+remove the WASM=0 parameters from the cmake configuration. 
+
+### Testing if I am compiling in an emscripten environment
 The flag ```__EMSCRIPTEN__``` says if I am under emscripten or not. Example:
 ```
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 ```
+
+### Seeing the emscripten directories
+Using the toolchain file or emcmake is not enough. I must add the include directories to the project using
+```include_directories("~/emsdk/emscripten/1.38.30/system/include")``` or the symbols won't be there in the world
+of c++
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
-#ifdef __EMSCRIPTEN__
+#include <SDL/SDL.h>
+//#ifdef __EMSCRIPTEN__
 #include <emscripten.h>
-#endif
-using namespace std;
+//#endif
+
+SDL_Surface *CreateSurface(unsigned int width, unsigned int height){
+    SDL_Surface  *screen = SDL_SetVideoMode(width,height, 32, SDL_SWSURFACE);
+    return screen;
+}
+
+void LockSurfaceIfNeeded(SDL_Surface* surface){
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_LockSurface(surface);
+    }
+}
+void DrawSomething(SDL_Surface* surface){
+    for (int i = 0; i < 256; i++) {
+        for (int j = 0; j < 256; j++) {
+            int alpha = (i+j) % 255;
+            *((Uint32*)surface->pixels + i * 256 + j) = SDL_MapRGBA(surface->format, i, j, 255-i, alpha);
+        }
+    }
+}
+void UnlockSurfaceIfNeeded(SDL_Surface* surface){
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_UnlockSurface(surface);
+    }
+}
 
 int main(int argc, char** argv){
-#ifdef __EMSCRIPTEN__
-    cout<<"__EMSCRIPTEN__ = true"<<endl;
-#endif
-    cout<<"loren ipsun"<<endl;
+    std::cout<<"Hello world"<<std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Surface  *screen = CreateSurface(256, 256);
+    LockSurfaceIfNeeded(screen);
+    DrawSomething(screen);
+    UnlockSurfaceIfNeeded(screen);
+    SDL_Flip(screen);
+    SDL_Quit();
     return 0;
 }
